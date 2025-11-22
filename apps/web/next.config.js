d4cf387fafd5f6a6580f3d53383d41d4b5b01b7f
andlopvic <andrea@axmc.xyz>
2025-11-22 20:57:36 +0100

    Ignore ESlint during build

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,9 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
   webpack: (config) => {
-    config.externals.push('pino-pretty', 'lokijs', 'encoding')
-    return config
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
   },
 };
 
